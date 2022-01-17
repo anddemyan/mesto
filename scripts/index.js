c4eeefbd86090683dd38a12c37a40d6e32e9c034
Andrey Demyanov <anddemyan@gmail.com>
2022-01-17 17:15:29 +0400

    Реализовать добавление карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,29 @@
+const initialCards = [
+  {
+    name: 'Ижевск',
+    link: './images/izhevsk.jpeg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 const popupOpenButton = document.querySelector('.profile__edit-button');
 const popupCloseButton = document.querySelector('.popup__close');
 const popup = document.querySelector('.popup');
@@ -6,6 +32,8 @@ let profileDescription = document.querySelector('.profile__description');
 let formElement = document.querySelector('.popup__form');
 let nameInput = document.querySelector('.popup__field_contact_name');
 let jobInput = document.querySelector('.popup__field_contact_job');
+const cardList = document.querySelector('.elements__list');
+const cardTemplate = document.querySelector('.card-template').content;
 
 //Функция открытия попапа с записью значений со страницы в поля формы
 function openPopup() {
@@ -27,8 +55,19 @@ function formSubmitHandler (evt) {
     closePopup();
 }
 
+//Функция создания карточек из template
+initialCards.forEach(function(cardData){
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector('.elements__image');
+  const cardTitle = cardElement.querySelector('.elements__title');
+
+  cardTitle.textContent = cardData.name;
+  cardImage.alt = cardData.name;
+  cardImage.src = cardData.link;
+  
+  cardList.prepend(cardElement);
+});
+
 formElement.addEventListener('submit', formSubmitHandler); 
 popupOpenButton.addEventListener('click', openPopup);
 popupCloseButton.addEventListener('click', closePopup);
-
-
